49140c0b12e0fbe0d5f5397d1320f23aa99a65d8
Kristian Bønløkke <krbonlokke@gmail.com>
2025-01-28 08:01:43 +0100

    start commit, tirsdag

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,6 +7,7 @@ export default async function Home() {
   return (
     <>
       <h1 className="text-4xl p-6">Aktiviteter</h1>
+      Yeaah
       <ul className="p-6 flex flex-col gap-6 h-screen mb-20 overflow-y-scroll">
         {activities.map(activity => (
           <li key={activity.id}>
